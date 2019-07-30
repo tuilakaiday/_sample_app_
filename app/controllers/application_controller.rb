56ad20e0764e3a28789db4a2e10d7183288d2392
tuilakaiday <kai.jsone@gmail.com>
2019-07-30 13:30:08 +0700

    Chapter 3: Most Static Pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello
-    render html: "hello, world!"
+  before_action :set_locale
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
