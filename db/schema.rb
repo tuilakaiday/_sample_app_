8d47afe7013208f0d25f049c7484114f5397f624
tuilakaiday <kai.jsone@gmail.com>
2019-08-03 16:17:24 +0700

    Chapter 7: Sign up

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_08_02_040830) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
