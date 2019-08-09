c368a774dcec55bc76df287de7c7437e4e597791
tuilakaiday <kai.jsone@gmail.com>
2019-08-09 14:47:20 +0700

    Chapter 12: Password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,11 @@
 class UserMailer < ApplicationMailer
   def account_activation user
     @user = user
+    mail to: @user.email, subject: t(".account_activation.subject")
+  end
+
+  def password_reset user
+    @user = user
+    mail to: @user.email, Subject: t("password_reset.subject")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
     resources :users
     resources :account_activations, only: :edit
+    resources :password_resets, except: :destroy
 
   end
 end
